6a1a50274a08efc9b0cc606ed8615ed4a833d35c
Dmitry <53238312+Herceg00@users.noreply.github.com>
2019-11-13 22:33:45 +0300

    Add files via upload

--- monte-carlo_method.c
+++ monte-carlo_method.c
@@ -2,18 +2,17 @@
 #include <omp.h>
 #include <stdlib.h>
 #include "time.h"
-
+#include "sys/time.h"
 
 
 int main(int argc, char **argv) {
-
     if (argc < 7) {
-        printf("Not enough arguments");
+        printf(" Not enough arguments");
         return -1;
     }
     int a = atoi(argv[1]);
     int b = atoi(argv[2]);
-    if( a >= b){
+    if (a >= b) {
         printf("Wrong segment");
         return -1;
     }
@@ -21,54 +20,41 @@ int main(int argc, char **argv) {
     int N = atoi(argv[4]);
     double p = strtod(argv[5], NULL);
     int P = atoi(argv[6]);
-
-    //srand(RAND_MAX);
+    struct timeval monte_start, monte_stop;
     omp_set_num_threads(P);
-    double born=0,single_lifetime=0,sum_lifetime=0, monte_start=0,monte_end=0;
+    double single_lifetime = 0, sum_lifetime = 0,
+    time_init = time(NULL);
     int sum_reached_b = 0;
-    int *seeds =  (int *)calloc(P, sizeof(int));
-    for(int i=0;i<P;i++){
-        seeds[i] = omp_get_wtime();
-    }
-
-    monte_start = omp_get_wtime();
-#pragma omp parallel for schedule(dynamic,1) shared(a,b,x,p,N)
-    for (int i = 0; i < N; i++) {
-        int position  = x;
-        //srand(seeds[omp_get_thread_num()]);
-        born = omp_get_wtime();
-        while((position!=b) && (position!=a)) {
-
-
-            if ((rand_r(&seeds[omp_get_thread_num()]) % 1000) / 1000.0 >= p) {
-                position--;
-            } else {
-                position++;
+    gettimeofday(&monte_start,NULL);
+    int iters = 0;
+#pragma omp parallel default(none) shared(time_init) firstprivate(a, b, N, x, P, p) reduction (+:sum_reached_b,iters)
+    {
+        int seed_for_thread = omp_get_thread_num()+time_init;
+#pragma omp for
+        for (int i = 0; i < N; i++) {
+            int position = x;
+            while ((position != a) && (position != b)) {
+                iters++;
+                int num = rand_r(&seed_for_thread);
+                if ((float)num / (float)RAND_MAX > p)
+                    position--;
+                else
+                    position++;
             }
-        }
-        single_lifetime = omp_get_wtime() - born;
-
-#pragma omp critical
-        {
-            sum_lifetime +=single_lifetime;
-            if (position == b){
-               sum_reached_b++;
-            }
-
+                if (position == b) {
+                    sum_reached_b++;
+                }
         }
     }
-    monte_end = omp_get_wtime();
-    FILE * stats = fopen("stats.txt","r+");
+    gettimeofday(&monte_stop,NULL);
+    FILE * stats = fopen("stats.txt","a");
     if(stats == NULL){
         stats = stdout;
     }
-    fprintf(stats,"PROBABILITY OF REACHING B:%f\n",((float)sum_reached_b/(float)N));
-    fprintf(stats, "AVERAGE LIFETIME:%lf\n",sum_lifetime/N);
-    fprintf(stats,"METHOD LIFETIME:%lf\n",monte_end - monte_start);
+    fprintf(stats,"PROBABILITY OF REACHING B:%f\n",(float)sum_reached_b/(float)N);
+    fprintf(stats, "AVERAGE LIFETIME:%lf\n",(float)(iters*1.0)/(float)N);
+    fprintf(stats,"METHOD LIFETIME:%ld\n",(monte_stop.tv_sec - monte_start.tv_sec)*1000000 + monte_stop.tv_usec - monte_start.tv_usec);
     fprintf(stats, "SEGMENT: [%d,%d], INITIAL POSITION: %d ",a,b,x);
-    fprintf(stats,"NUMBER OF ELEMENTS: %d, ATOMIC PROBABILITY: %f, NUMBER OF THREADS: %d\n",N,p,P);
+    fprintf(stats,"NUMBER OF ELEMENTS: %d, ATOMIC PROBABILITY: %f, NUMBER OF THREADS: %d\n\n",N,p,P);
     fclose(stats);
-    free(seeds);
-
-
 }
