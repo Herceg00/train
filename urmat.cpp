392311809e709d9b9ae8b9582d837e972a2f1fd2
Dmitry <53238312+Herceg00@users.noreply.github.com>
2019-12-07 23:58:41 +0300

    Add files via upload

--- urmat.cpp
+++ urmat.cpp
@@ -4,7 +4,8 @@
 #include "assert.h"
 #include "stdlib.h"
 #include "string.h"
-
+#include "cublas.h"
+#include "/usr/local/cuda-10.1/include/cuda_runtime.h"
 
 
 double u(double x, double t){
@@ -45,10 +46,78 @@ int main(int argc,char** argv) {
 //h_steps = grid dimension on OX coordinate
 //tau_steps = grid gimaension on OY coordinate
 
+void indefinite_scheme_first_first(int tau_steps,int h_steps, double a){
+    double tau = 1.0/tau_steps; //atomic steps for every dimension
+    double h = 1.0/h_steps;
+    double *fi1,*fi2;
+    fi1 = (double*)calloc(tau_steps, sizeof(double));
+    fi2 = (double*)calloc(tau_steps,sizeof(double));
+    for(int i = 0;i<tau_steps;i++){
+        fi1[i] = u(0,i*tau);
+        fi2[i] = u(1,i*tau);
+    }
+    double **grid = (double**) calloc(sizeof(double *),2);
+    grid[0] = (double*) calloc(h_steps,sizeof(double));
+    grid[1] = (double*) calloc(h_steps, sizeof(double));
+    for(int i = 0;i<h_steps;i++){
+        grid[0][i] = u(i*h,0);
+    }
+    //this values are constant for the whole scheme
+    double A_i = a*a/h*h;
+    double B_i = a*a/h*h;
+    double C_i = 2*a*a/h*h + 1/tau;
+
+    double hi_1 = 0;
+    double mu_1;
+    double mu_2;
+    double hi_2 = 0;
+
+    //Прогоночные коэффициенты
+    double *alpha,*betta;
+    alpha = (double*)calloc(h_steps, sizeof(double));
+    betta = (double*)calloc(h_steps, sizeof(double));
+
+    alpha[0] = 0;
+    for(int i = 1;i<tau_steps;i++){ //i = layer which we fill, standing on i-1 layer
+        betta[0] = fi1[i];
+        for(int j=1;j<h_steps;j++){
+            alpha[j] = B_i/(C_i - alpha[j-1]*A_i);
+            betta[j] = (A_i*betta[j-1]+((grid[(i-1)%2][j])/tau + f(j*h,i*tau))) /(C_i - alpha[i-1]*A_i);
+        }
+        grid[(i%2)][h_steps - 1] = (fi2[i]);
+        for(int j = h_steps -2;j>-1;j--){
+            grid[i%2][j] = alpha[j+1]*grid[i%2][j+1] + betta[i+1];
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
+void indefinite_scheme_second(int tau_steps,int h_steps, double a){
+
+}
+void indefinite_scheme_first_second(int tau_steps,int h_steps, double a){
+
+}
+void indefinite_scheme_second_first(int tau_steps,int h_steps, double a){
+
+
+}
+
+
+
+
 void definite_scheme_first_first(int tau_steps,int h_steps, double a){
 
     //DOING INITIAIZATION
-
     double tau = 1.0/tau_steps; //atomic steps for every dimension
     double h = 1.0/h_steps;
     double *fi1,*fi2;
